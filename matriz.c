fc5db4aadbf887a4c793232898c483f3aa718dc0
Emil Peñaló <emil@Emils-MacBook-Pro.local>
2020-11-17 01:08:14 -0400

    Too tired to do actual comments

--- matriz.c
+++ matriz.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <conio.c>
+#include <time.h>
+#include <conio.c>
 
 #define MAX 15
 #define MIN 3
 
-#define XOUT 1
-#define YOUT 1
-#define INBW 3
+#define XOUT 2
+#define YOUT 2
+#define INBW 4
 
-// #define CB BLUE
-// #define CF LIGHTGRAY
-//
-// #define CBA LIGHTBLUE
-// #define CFA DARKGRAY
+#define CB BLUE
+#define CLG LIGHTGRAY
+
+#define SELECT YELLOW
+
+#define ESC 27
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
+#define LEFT 75
+
 
 
 int checkOrden(int[], int);
-// void cuadroOut(int [], int, int);
-// void setcolor(int, int);
-// void colordefault();
+void populate(int[][MAX], int);
+int randrange(int, int);
+int inArr(int[][MAX], int, int);
+void cuadroOut(int [][MAX], int);
+void setcolor(int, int);
+void colordefault();
+void gamestart(int[][MAX], int);
 
 int main() {
    srand(time(NULL));
-   
+
    int size;
 
    do {
@@ -36,58 +47,130 @@ int main() {
 
    } while(size < MIN || size > MAX);
 
-   int nums[size][size];
+   int nums[size][MAX];
+   nums[size-1][size-1] = 0;
 
-   // lim = (size*size)-1;
+   populate(nums, size);
 
-   // for (int i = 0; i < lim; i++) {
-   //    do {
-   //       n = rand() % lim;
-   //    } while(selected[n]);
-   //
-   //    nums[i] = n+1;
-   //    selected[n] = 1;
-   // }
-
-   // cuadroOut(nums, lim, size);
-
-   // for (int i = 0; i < lim; i++) {
-   //    if (!(i % size)) {
-   //       printf("\n");
-   //    }
-   //    printf("%3d ", nums[i]);
-   // }
-
-
-   // int arr1[9] = {1,2,3,4,5,6,7,8,9};
-   // int arr2[9] = {0,2,3,4,5,6,7,8,9};
-   // int arr3[9] = {1,2,4,7,5,6,3,8,9};
-   // int arr4[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
-   //
-   // printf("1: %d\n", checkOrden(arr1, 9));
-   // printf("0: %d\n", checkOrden(arr2, 9));
-   // printf("0: %d\n", checkOrden(arr3, 9));
-   // printf("1: %d\n", checkOrden(arr4, 12));
+   gamestart(nums, size);
 
    return 0;
 }
 
-// void cuadroOut(int arr[], int n, int size) {
-//    clrscr();
-//
-//    for (int i = 0; i < n; i++) {
-//       if (!(i % size)) {
-//          printf("\n");
-//       }
-//       if (i % 2) {
-//          setcolor(CB,CF);
-//       } else {
-//          setcolor(CBA, CFA);
-//       }
-//       printf("%3d ", arr[i]);
-//    }
-//    colordefault();
-// }
+/*
+   Funcion: populate
+   Argumentos: (int) arr[][]. Arreglo de enteros.
+               (int) size. La cantidad de elementos en arr[].
+   Objetivo: Popular el arreglo con numeros aleatorios del 1 a su limite
+   Retorno: void
+*/
+void gamestart(int arr[][MAX], int size) {
+   int key, x = size-1, y = size-1;
+   int lastx = x, lasty = y;
+
+   clrscr();
+   do {
+      cuadroOut(arr, size);
+
+      key = getch();
+
+      if (key == UP) {
+         y--;
+      }
+
+      if (key == DOWN) {
+         y++;
+      }
+
+      if (key == LEFT) {
+         x--;
+      }
+
+      if (key == RIGHT) {
+         x++;
+      }
+
+      arr[lasty][lastx] = arr[y][x];
+      arr[y][x] = 0;
+
+      lastx = x;
+      lasty = y;
+
+   } while(key != ESC);
+}
+
+/*
+   Funcion: populate
+   Argumentos: (int) arr[][]. Arreglo de enteros.
+               (int) size. La cantidad de elementos en arr[].
+   Objetivo: Popular el arreglo con numeros aleatorios del 1 a su limite
+   Retorno: void
+*/
+void cuadroOut(int arr[][MAX], int size) {
+   int y = YOUT;
+   clrscr();
+
+   for (int i=0; i < size; i++) {
+      for (int e=0; e < size; e++) {
+         if (!e) {
+            gotoxy(XOUT, y);
+         } else {
+            gotoxy(XOUT+(e*INBW), y);
+         }
+
+         if (arr[i][e]) {
+            setcolor(CB, CLG);
+         } else {
+            setcolor(SELECT, SELECT);
+         }
+
+         printf("%3d ", arr[i][e]);
+      }
+      y++;
+   }
+   colordefault();
+}
+
+/*
+   Funcion: populate
+   Argumentos: (int) arr[][]. Arreglo de enteros.
+               (int) size. La cantidad de elementos en arr[].
+   Objetivo: Popular el arreglo con numeros aleatorios del 1 a su limite
+   Retorno: void
+*/
+void populate(int arr[][MAX], int size) {
+   int n;
+
+   for (int i=0; i < size; i++) {
+      for (int e=0; e < size; e++) {
+         if (!(i == size-1 && e == size-1 )) {
+            do {
+               n = randrange(1, (size*size)-1);
+            } while(inArr(arr, size, n));
+            arr[i][e] = n;
+         }
+      }
+   }
+}
+
+/*
+   Funcion: inArr
+   Argumentos: (int) arr[][]. Arreglo de enteros
+               (int) size. Tamaño del arreglo
+               (int) n. numero a buscar en arr
+   Objetivo: Determinar si un numero esta en un arreglo
+   Retorno: 1 o 0 dependiendo si el numero esta en el arreglo o no
+*/
+int inArr(int arr[][MAX], int size, int n) {
+   for (int i=0; i < size; i++) {
+      for (int e=0; e < size; e++) {
+         if (arr[i][e] == n) {
+            return 1;
+         }
+      }
+   }
+   return 0;
+}
 
 /*
    Funcion: checkOrden
@@ -107,7 +190,14 @@ int checkOrden(int arr[], int n) {
    return 1;
 }
 
-int randrange(int a,int b) {
+/*
+   Funcion: checkOrden
+   Argumentos: (int) arr[]. Arreglo de enteros.
+               (int) n. La cantidad de elementos en arr[].
+   Objetivo: Determinar so arr[] esta ordenado o no.
+   Retorno: (int) 1 si el arreglo esta ordenado, 0 si no lo esta.
+*/
+int randrange(int a, int b) {
    return rand() % ( b - a + 1 ) + a;
 }
 
@@ -118,10 +208,10 @@ int randrange(int a,int b) {
    Objetivo: Determinar so arr[] esta ordenado o no.
    Retorno: (int) 1 si el arreglo esta ordenado, 0 si no lo esta.
 */
-// void setcolor(int ct,int cf) {
-//    textbackground(cf);
-//    textcolor(ct);
-// }
+void setcolor(int ct,int cf) {
+   textbackground(cf);
+   textcolor(ct);
+}
 
 /*
    Funcion: checkOrden
@@ -130,6 +220,6 @@ int randrange(int a,int b) {
    Objetivo: Determinar so arr[] esta ordenado o no.
    Retorno: (int) 1 si el arreglo esta ordenado, 0 si no lo esta.
 */
-// void colordefault() {
-//    setcolor(LIGHTGRAY,BLACK);
-// }
+void colordefault() {
+   setcolor(LIGHTGRAY,BLACK);
+}
